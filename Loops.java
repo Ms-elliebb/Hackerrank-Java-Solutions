38be1352e0961733a615bf141d9071d013443b73
ellie <74204751+Ms-elliebb@users.noreply.github.com>
2024-02-22 00:41:14 +0300

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+
+public class Solution{
+    public static void main(String[] args){
+     Scanner sc=new Scanner(System.in);
+     
+     int j=sc.nextInt();
+     for(int i=1;i<=10;i++){
+       int result=j*i;
+       System.out.println(j+" x "+i+" = "+result); 
+         
+     }
+    sc.close();
+    
+    }
+}
